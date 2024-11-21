779342472d3b97c4a9172fd09c5c640f796bec5d
tangpaak <139091650+tangpaak@users.noreply.github.com>
2024-11-22 00:38:29 +0800

    update

--- server.js
+++ server.js
@@ -3,15 +3,31 @@ const { parse } = require('url')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
+const hostname = 'localhost'
+const port = 3000
+const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url, true)
-    handle(req, res, parsedUrl)
-  }).listen(3000, (err) => {
+  createServer(async (req, res) => {
+    try {
+      // Parse URL
+      const parsedUrl = parse(req.url, true)
+      
+      // Let Next.js handle the request
+      await handle(req, res, parsedUrl)
+    } catch (err) {
+      console.error('Error occurred handling', req.url, err)
+      res.statusCode = 500
+      res.end('internal server error')
+    }
+  })
+  .once('error', (err) => {
+    console.error(err)
+    process.exit(1)
+  })
+  .listen(port, (err) => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log(`> Ready on http://${hostname}:${port}`)
   })
 })
